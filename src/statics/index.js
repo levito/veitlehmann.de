bb17a1849cd6acfb474940cf14668ef1c620fbed
Veit Lehmann <levito82@gmail.com>
2022-12-07 23:55:59 +0100

    fix content not scrollable

--- src/statics/index.js
+++ src/statics/index.js
@@ -1,14 +1,41 @@
-(function () {
-  var cssSupports = (window.CSS || {}).supports || function () {};
+;(function () {
+  var cssSupports = (window.CSS || {}).supports || function () {}
 
-  if (cssSupports('transform-style', 'preserve-3d')) {
-    return;
+  if (!cssSupports('transform-style', 'preserve-3d')) {
+    var sections = document.querySelectorAll('.js_section')
+    var targets = document.querySelectorAll('.js_target')
+
+    ;[].slice.call(targets).forEach(function (target, i) {
+      sections[i].appendChild(target)
+    })
+
+    return
   }
 
-  var sections = document.querySelectorAll('.js_section');
-  var targets = document.querySelectorAll('.js_target');
+  var html = document.documentElement
+  var sectionBodies = document.querySelectorAll('.js_section__body')
 
-  [].slice.call(targets).forEach(function (target, i) {
-    sections[i].appendChild(target);
-  });
-})();
+  document.addEventListener('transitionend', function (e) {
+    if (!e.target.matches('a')) {
+      sectionBodies.forEach(function (sectionBody) {
+        sectionBody.scrollTo(0, 0)
+      })
+
+      html.classList.add('is-flat', 'is-static')
+    }
+  })
+
+  document.addEventListener('click', function (e) {
+    var href = e.target.getAttribute('href')
+
+    if (e.target.matches('a') && href[0] === '#') {
+      e.preventDefault()
+      html.classList.remove('is-flat')
+
+      setTimeout(function () {
+        html.classList.remove('is-static')
+        window.location.hash = href.substr(1)
+      }, 0)
+    }
+  })
+})()
