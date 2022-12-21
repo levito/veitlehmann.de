91143812e62e3427155be0fb0d6588a4d3124bc0
Veit Lehmann <levito82@gmail.com>
2022-12-21 14:30:53 +0100

    prepare collections

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,16 @@
-export const slugify = (string: String = '') =>
+import type { SectionData } from './components/Section.astro'
+
+export const slugify = (string: string = '') =>
   string.toLowerCase().replace(/\s/g, '-')
+
+export const getContent = async () => {
+  const sections = await Promise.all(
+    Object.values(import.meta.glob<SectionData>(`./content/*.md*`)).map(
+      (resolve) => resolve(),
+    ),
+  )
+  const links = sections.map(({ frontmatter }) => frontmatter?.title)
+  const year = new Date().getFullYear()
+
+  return { links, sections, year }
+}
